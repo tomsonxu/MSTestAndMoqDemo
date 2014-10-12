35ebf034920a91a3af2076e888276651565aaf2b
Tomson Xu <tomson.tao.xu@gmail.com>
2014-10-13 01:04:55 +0800

    init version > add mstest simplest test demo > add mstest exception test demo

---
+++ MSTestAndMoqDemo/BankAccountTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestAndMoqDemo.TestedClasses;
+
+namespace MSTestAndMoqDemo
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        //normal test
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        /*
+         * !!!exception test
+         * there are 2 ways to test exception (2nd one is preferred):
+         *  > use "ExpectedException" annotation. 
+         *      The drawback of this approach is that we just know exception is thrown, 
+         *      but don't know which condition throws it
+         *  
+         *  > use "try/catch" in test method to catch exception
+         *      assert exception under "catch" and return, assert fail after "catch"
+         *      
+         */
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, "Debit amount exceeds balance");
+                return;
+            }
+
+            Assert.Fail("No exception was thrown.");
+        }
+    }
+}
---
+++ MSTestAndMoqDemo/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestAndMoqDemo.TestedClasses;
+
+namespace MSTestAndMoqDemo
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            int a = 1;
+            int b = 2;
+
+            //act
+            int result = calculator.add(a, b);
+
+            //assert
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
